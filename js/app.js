d8b383af4d9bfdf4833c79afc02f536adda9df97
Diego <dleonven@gmail.com>
2017-11-29 12:19:35 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -6,15 +6,15 @@ const dt = 0.017
 
 //this method returns a random posible starting position for the enemies
 function posibleStartingPosition(){
-    let startingPosition;
-    const aux = Math.floor(Math.random() * 3) + 1;
-    if(aux == 1){
-        startingPosition = 60
-    } else if (aux == 2) {
-        startingPosition = 145
-    } else startingPosition = 230
+  let startingPosition;
+  const aux = Math.floor(Math.random() * 3) + 1;
+  if(aux == 1){
+    startingPosition = 60
+  } else if (aux == 2) {
+    startingPosition = 145
+  } else startingPosition = 230
 
-    return startingPosition;
+  return startingPosition;
 }
 
 //this method returns a random between the constants declared before
@@ -24,48 +24,48 @@ function getSpeed() {
 
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
 
-    //if the enemy is still on the canvas, then move it
-    if(this.x < 505){
-        this.x += this.speed * dt;
-    }
+  //if the enemy is still on the canvas, then move it
+  if(this.x < 505){
+    this.x += this.speed * dt;
+  }
 
-    //if it's outside, then set to a random posible starting position, with a random new speed
-    else{
-        this.x = -100;
-        this.y = posibleStartingPosition();
-        this.speed = getSpeed()*dt;
-    }
+  //if it's outside, then set to a random posible starting position, with a random new speed
+  else{
+    this.x = -100;
+    this.y = posibleStartingPosition();
+    this.speed = getSpeed()*dt;
+  }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function(x, y){
-    this.x = x;
-    this.y = y;
-    this.sprite = 'images/char-boy.png';
+  this.x = x;
+  this.y = y;
+  this.sprite = 'images/char-boy.png';
 };
 
 Player.prototype.update = function(){
@@ -79,44 +79,44 @@ Player.prototype.update = function(){
 };
 
 Player.prototype.render = function(){
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 Player.prototype.handleInput = function(e){
   //posible player movements
-    if (e == "left") {
-      if(player.x > 3){
-        player.x -= 100;
-      }
+  if (e == "left") {
+    if(player.x > 3){
+      player.x -= 100;
     }
-    else if (e == "right") {
-      if(player.x < 403){
-        player.x += 100;
-      }
+  }
+  else if (e == "right") {
+    if(player.x < 403){
+      player.x += 100;
     }
-    else if (e == "up") {
-      player.y -= 85;
+  }
+  else if (e == "up") {
+    player.y -= 85;
 
-      //if the player reaches the water, go back to the starting point
-      if(player.y < 60){
-        player.x = 203;
-        player.y = 400;
-      }
+    //if the player reaches the water, go back to the starting point
+    if(player.y < 60){
+      player.x = 203;
+      player.y = 400;
     }
-    else if (e == "down") {
-      if(player.y < 400){
-        player.y += 85;
-      }
+  }
+  else if (e == "down") {
+    if(player.y < 400){
+      player.y += 85;
     }
+  }
 };
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [
-    new Enemy(-100,posibleStartingPosition(),dt*getSpeed()),
-    new Enemy(-100,posibleStartingPosition(),dt*getSpeed()),
-    new Enemy(-100,posibleStartingPosition(),dt*getSpeed())
+  new Enemy(-100,posibleStartingPosition(),dt*getSpeed()),
+  new Enemy(-100,posibleStartingPosition(),dt*getSpeed()),
+  new Enemy(-100,posibleStartingPosition(),dt*getSpeed())
 ];
 
 // Place the player object in a variable called player
@@ -128,11 +128,11 @@ var player = new Player(203,400);
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-    player.handleInput(allowedKeys[e.keyCode]);
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
+  player.handleInput(allowedKeys[e.keyCode]);
 });
